7720633d10b82295a9a194bf8e6fdd442c52ce20
Aly Abdelrahman <abdelrahmanaly5@gmail.com>
2019-10-26 17:33:41 -0400

    created Shifts Model,Usershift model

--- ACGCrossingApp/livedata/models.py
+++ ACGCrossingApp/livedata/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class UserShift(models.Model):
+    shift = models.ForeignKey() #to do later
+    shift_user = models.ForeignKey(
+        get_user_model(),
+    )
+    checked_in = models.BooleanField()
+    
+    def __str__(self):
+        return self
+
+    
