cb636307db898d51f9f508cae0b75010092b20ec
Jess Johnson <jess@wikileaf.com>
2019-10-26 11:58:46 -0400

    add channels for websockets

--- ACGCrossingApp/ACGCrossingApp/settings.py
+++ ACGCrossingApp/ACGCrossingApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'src',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = 'ACGCrossingApp.routing.application'
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # acg-crossing-app
 ACG Crossing Guard App (RHoK #11)
+
+### Basic Run Instructions for now
+
+```bash
+cd acg-crossing-app/ACGCrossingApp/
+python3 manage.py runserver
+```
