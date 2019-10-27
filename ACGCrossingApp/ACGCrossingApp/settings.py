6dbb5c89b32f754dccef4cac3378c61fac2e69e1
charles <4837704+charlesej@users.noreply.github.com>
2019-10-27 11:13:14 -0400

    Remove unused celery code

--- ACGCrossingApp/ACGCrossingApp/settings.py
+++ ACGCrossingApp/ACGCrossingApp/settings.py
@@ -163,9 +163,3 @@ REST_FRAMEWORK = {
     )
 }
 
-# Celery application definition
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER = 'json'
