32ba8df2838bcd2814815bfb48961aecab1a7669
tmpdubz <t.m.dubiel@gmail.com>
2020-01-05 16:48:21 -0500

    Removed debug mode from docker scripts
    - we probably don't need it right now
    - we can add it back in if we ever put this into production and have a real build pipeline

--- docker/dev/environment/scripts/entrypoint.sh
+++ docker/dev/environment/scripts/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-set -ex
+set -e
 
 ARG="$1"
 
 case "${ARG}" in
     # For updating the pipfile.lock in the project when a new dependency is added
     pipfile)
+        echo "Updating Pipfile..."
         CMD=("/scripts/copy_pipfiles.sh")
         ;;
     # For running any of the django management commands
     manage)
+        echo "Django manage.py command provided, passing args through to manage.py..."
         shift 1
         CMD=("/usr/local/bin/python" "/live-app/manage.py" "${@}")
         ;;
